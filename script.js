6a41d898db09d42f4aee3163766249f7e8d09a50
Thomas R McNutt <pachown@gmail.com>
2020-03-11 11:41:05 -0600

    Bug Fixes

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ const rockBeatsScissorsLoss = ['You Are Taking The Loss Like A Champion', 'That 
 const scissorsBeatsPaperLoss = ['You Are Taking The Loss Like A Champion', 'That AI Will Pay!', 'Can\'t Win Them All', 'They Cut You In Half. Magic Trick Gone Wrong', 'Your Got Scissored', 'Cut Up And Crying Like Onions']
 const paperBeatsRockLoss = ['You Are Taking The Loss Like A Champion', 'That AI Will Pay!', 'Can\'t Win Them All', 'You were Covered In Defeat', 'It Was Horrible, No One Could See you', 'How Do You See With That Thing On?']
 
-let victories = 0
-let defeats = 0
-let draws = 0
-
-const computerChoices = ['Rock', 'Paper', 'Scissors']
+let victories = 0;
+let defeats = 0;
+let draws = 0;
 
 const playerChoices = document.querySelector('[player-choices]');
 
 playerChoices.addEventListener('click', e => {
-    document.getElementById('rock').style.backgroundColor = 'white';
-    document.getElementById('paper').style.backgroundColor = 'white';
-    document.getElementById('scissors').style.backgroundColor = 'white';
+    document.getElementById('rock').style.backgroundColor = 'rgb(180, 179, 179)';
+    document.getElementById('paper').style.backgroundColor = 'rgb(180, 179, 179)';
+    document.getElementById('scissors').style.backgroundColor = 'rgb(180, 179, 179)';
 
     let playerChoice = '';
     if (e.target.tagName.toLowerCase() === 'input') {
+        document.getElementById('playerChoiceBox').innerHTML = playerChoice;
+        document.getElementById('victories').innerHTML = victories;
+        document.getElementById('defeats').innerHTML = defeats;
+        document.getElementById('draws').innerHTML = draws;
+
         if (e.target.id === 'rock') {
             playerChoice = 'Rock';
             document.getElementById('rock').style.backgroundColor = 'red';
@@ -32,20 +35,16 @@ playerChoices.addEventListener('click', e => {
             playerChoice = 'Scissors';
             document.getElementById('scissors').style.backgroundColor = 'red';
         }
-
         let computerChoice = randomChoice();
-        let resultsDisplayText = WinnerLogic(playerChoice, computerChoice);
-
-        document.getElementById('results-container').innerHTML = resultsDisplayText;
-        document.getElementById('playerChoiceBox').innerHTML = playerChoice;
         document.getElementById('computerChoiceBox').innerHTML = computerChoice;
-        document.getElementById('victories').innerHTML = victories;
-        document.getElementById('defeats').innerHTML = defeats;
-        document.getElementById('draws').innerHTML = draws;
+        let resultsDisplayText = winnerLogic(playerChoice, computerChoice);
+        document.getElementById('results-container').innerHTML = resultsDisplayText;
     }
 })
 
 var randomChoice = () => {
+    const computerChoices = ['Rock', 'Paper', 'Scissors'];
+
     let computerChoice = computerChoices[Math.floor(Math.random() * 3)];
     
         document.getElementById('computerrock').style.backgroundColor = 'white';
@@ -89,15 +88,3 @@ winnerLogic = function(playerChoice, computerChoice) {
         return rockBeatsScissorsLoss[Math.floor(Math.random() * rockBeatsScissorsLoss.length)];
     }
 }
-
-const tieCatchPhrases = ['Cats Game', 'Ties Are Meant For The Office', 'Another Tie!?!', 'Boring Tie', 'At Least You Didn\'t Lose!', 'Just A Tie']
-const rockBeatsScissors = ['You Crushed The AI', 'Always Beating The Odds', 'I Wonder If You Could Win ANOTHER Game?', 'Edward Scissorhands Has Nightmares About Your Rock', 'Crushing That Weak Scissor Player!']
-const scissorsBeatsPaper = ['You Crushed The AI', 'Always Beating The Odds', 'I Wonder If You Could Win ANOTHER Game?', 'He Was Made Of Paper?! Oh The Humanity!', 'You Are A Chef, Slicing And Dicing', 'Chop It Up To Good Luck', 'Cut That Nonsense']
-const paperBeatsRock = ['You Crushed The AI', 'Always Beating The Odds', 'I Wonder If You Could Win ANOTHER Game?', 'That Rock Might Never See Again', 'Rocky Cant Pull Punches On Your Piece Of Paper', 'It\'s A Cover Up, Detective!']
-const rockBeatsScissorsLoss = ['You Are Taking The Loss Like A Champion', 'That AI Will Pay!', 'Can\'t Win Them All', 'Your Scissors Got Smashed!', 'Stuck Between A Rock And A Hard Place', 'Crushing Defeat']
-const scissorsBeatsPaperLoss = ['You Are Taking The Loss Like A Champion', 'That AI Will Pay!', 'Can\'t Win Them All', 'They Cut You In Half. Magic Trick Gone Wrong', 'Your Got Scissored', 'Cut Up And Crying Like Onions']
-const paperBeatsRockLoss = ['You Are Taking The Loss Like A Champion', 'That AI Will Pay!', 'Can\'t Win Them All', 'You were Covered In Defeat', 'It Was Horrible, No One Could See you', 'How Do You See With That Thing On?']
-
-var victories = 0
-var defeats = 0
-var draws = 0
